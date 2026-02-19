2e17cf8baba739c26946bfc1f93f791f7f851332
narin10101014 <narin10101014@gmail.com>
2026-02-19 08:20:15 +0700

    เพิ่ม /api/login และ เปลี่ยนชื่อ database

--- src/main/java/com/narin/ecommerce/controller/AuthController.java
+++ src/main/java/com/narin/ecommerce/controller/AuthController.java
@@ -1,4 +1,21 @@
 package com.narin.ecommerce.controller;
 
+import com.narin.ecommerce.dto.LoginRequest;
+import com.narin.ecommerce.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    AuthService authService;
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest req) {
+        return authService.login(req.getUsername(), req.getPassword());
+    }
 }
--- src/main/java/com/narin/ecommerce/service/impl/AuthServiceImpl.java
+++ src/main/java/com/narin/ecommerce/service/impl/AuthServiceImpl.java
@@ -1,19 +1,34 @@
 package com.narin.ecommerce.service.impl;
 
 import com.narin.ecommerce.entity.User;
+import com.narin.ecommerce.repository.UserRepository;
 import com.narin.ecommerce.service.AuthService;
+import com.narin.ecommerce.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
 
     @Override
     public void register(User user) {
 
     }
 
-    @Override
     public String login(String username, String password) {
-        return null;
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!encoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+        return JwtUtil.generateToken(username);
     }
 }
